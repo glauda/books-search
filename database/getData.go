825a3fbb4fa552d2e4724c3164050efb203ac2b2
glauda <florent-glauda@orange.fr>
2020-11-29 15:05:39 -0500

    get result of the first API call

--- database/getData.go
+++ database/getData.go
@@ -4,74 +4,81 @@ import (
     //"bytes"
     //"encoding/json"
     "fmt"
-//    "io/ioutil"
-//	"net/http"
+    "io/ioutil"
+	"net/http"
 	"crypto/md5"
-	//"math/rand"
-	//"time"
-	//"strconv"
+	"crypto/rand"
+	"encoding/hex"
 )
-/*
-const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-func RandStringBytes(n int) string {
-    b := make([]byte, n)
-    for i := range b {
-        b[i] = letterBytes[rand.Intn(len(letterBytes))]
-    }
-    return string(b)
+func GenRandomBytes(size int) (blk []byte, err error) {
+    blk = make([]byte, size)
+	_, err = rand.Read(blk)
+    return
+}
+
+/*	// generate random timestamp
+ts, err := GenRandomBytes(10)
+
+if err != nil {
+	panic(err)
+} else {
+	sblk := string(blk)
+	fmt.Printf("sblk: %s\n", sblk)
+	fmt.Printf("hex : %x\n", blk)
 }
 */
-func main() {
-	
-	// TODO: mettre des strings?
 
-	// http://gateway.marvel.com/v1/public/comics?ts=10&apikey=acc1c4efc8246d5b7ff81d2b9b2e00ce&hash=900246b409580e3ac228b27e13977a2b
+func CreateAPIRequest(path string, option string) (request string){
+	
+	fmt.Printf("Creating request for %s \n", path)
 
 	// API variables 
 	publicKey := []byte("acc1c4efc8246d5b7ff81d2b9b2e00ce")
 	privateKey := []byte("67909a94a20b1231835d360b55d26c16634ee256")
 	ts := []byte("10")
-	//ts := []byte(strconv.Itoa(time.Now().Unix()))
+
+	hash := md5.Sum( append( append(ts, privateKey...), publicKey...) )
+
+	domainName := "https://gateway.marvel.com:443" 
+
+	// log messages
+	fmt.Printf("ts=%s \n", ts)
+	fmt.Printf("apikey=%s \n", publicKey)
+	fmt.Printf("hash=%x \n", hash)
+
+	request = domainName + path + "?" +
+		option + 
+		"&ts=" + string(ts) +
+		"&apikey=" + string(publicKey) +
+		"&hash=" + string(hex.EncodeToString(hash[:]))
+
+	return 
+}
+
+
+func main() {
+	
+	// http://gateway.marvel.com/v1/public/comics?ts=10&apikey=acc1c4efc8246d5b7ff81d2b9b2e00ce&hash=900246b409580e3ac228b27e13977a2b
 
 	fmt.Println("Starting the application...")
 
-	fmt.Printf("ts: %s \n", ts)
 	
-
-	//fmt.Printf("%s \n", append( append(ts, privateKey...), publicKey...) )
-
-	//data := []byte("These pretzels are making me thirsty.")
-	//fmt.Printf("%x \n", md5.Sum( append( append(ts, privateKey...), publicKey...) ))
-
-	hash := md5.Sum( append( append(ts, privateKey...), publicKey...) )
-	fmt.Printf("%x \n", hash)
-
-
 	// GET request
-	/*
-	response, err := http.Get("https://httpbin.org/ip")
+	
+	path, option := "/v1/public/series", "limit=50"
+	//fmt.Printf(CreateAPIRequest(path, option))
+	
+	response, err := http.Get(CreateAPIRequest(path, option))
 	
 	if err != nil {
         fmt.Printf("The HTTP request failed with error %s\n", err)
     } else {
-        data, _ := ioutil.ReadAll(response.Body)
-        fmt.Println(string(data))
+		data, _ := ioutil.ReadAll(response.Body)
+		_ = ioutil.WriteFile("./data/resGetSeries.json", data, 0644)
+		//fmt.Println(string(data))
+		
     }
-	*/
-
-	/*
-	jsonData := map[string]string{"firstname": "Nic", "lastname": "Raboy"}
-    jsonValue, _ := json.Marshal(jsonData)
 	
-	response, err = http.Post("https://httpbin.org/post", "application/json", bytes.NewBuffer(jsonValue))
-	
-	if err != nil {
-        fmt.Printf("The HTTP request failed with error %s\n", err)
-    } else {
-        data, _ := ioutil.ReadAll(response.Body)
-        fmt.Println(string(data))
-    }
-	*/	
 	fmt.Println("Terminating the application...")
 }
